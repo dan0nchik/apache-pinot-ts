ac4e3ba0c4337bb30ddb450566aa647f42621d56
Daniel Khromov <danielkhromov@gmail.com>
2024-04-28 20:19:32 +0300

    minimizing hardcoded value

--- scripts/create_companies_segment.sh
+++ scripts/create_companies_segment.sh
@@ -1,6 +1,6 @@
 docker run --rm -ti \
-    --network=apache-pinot-ts_default \
-    -v ~/dev/apache-pinot-ts:/tmp/pinot \
+    --network=pinot \
+    -v $(pwd):/tmp/pinot \
     --name pinot-data-ingestion-job \
     apachepinot/pinot:latest LaunchDataIngestionJob \
     -jobSpecFile /tmp/pinot/ingestion_configs/companies-batch-job-spec.yml
--- scripts/upload_companies_schema.sh
+++ scripts/upload_companies_schema.sh
@@ -1,6 +1,6 @@
 docker run --rm -ti \
-    --network=apache-pinot-ts_default \
-    -v ~/dev/apache-pinot-ts:/tmp/pinot \
+    --network=pinot \
+    -v $(pwd):/tmp/pinot \
     --name pinot-batch-table-creation \
     apachepinot/pinot:latest AddTable \
     -schemaFile /tmp/pinot/schemas/companies_schema.json \
