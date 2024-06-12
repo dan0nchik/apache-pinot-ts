c29b7701c39338983696d8e9efbb1187e8f6b0f3
Daniel Khromov <danielkhromov@gmail.com>
2024-06-13 02:56:24 +0300

    Update model.py

--- streamlit/model.py
+++ streamlit/model.py
@@ -50,7 +50,7 @@ def train_and_evaluate(df, steps_ahead):
 
     # Split into training and test sets
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42
+        X, y, test_size=0.3, shuffle=False
     )
 
     # Train the model
